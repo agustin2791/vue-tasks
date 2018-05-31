401038ffa1432c3fe264211cda0a210e6549b092
Jesus Diaz-Barriga <jagustin2791@gmail.com>
2018-05-31 14:38:54 -0700

    redirects

--- server.js
+++ server.js
@@ -6,6 +6,9 @@ const serveStatic = require('serve-static');
 const app = express();
 // app.use(secure)
 app.use("/", serveStatic ( path.join (__dirname + '/' ) ) );
+app.use("*", function(req, res) {
+  res.sendFile(__dirname + '/index.html')
+})
 
 const port = process.env.PORT || 5000;
 app.listen(port);
